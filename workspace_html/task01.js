1a552699ed79ebdd4a76ff96e3d1219c0aac2018
syyyyp <christieforsy@gmail.com>
2024-09-11 21:53:29 +0900

    20240911 박서연

---
+++ workspace_html/task01.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("input[type='button']");
+const nav = document.querySelector("nav");
+
+const todoForm = document.getElementById('todo-form')
+// function changeColor() {
+//   name.style.color = "blue";
+// }
+
+input.addEventListener("click", () => {
+  const input=prompt("추가할 할일을 입력하세요");
+});
